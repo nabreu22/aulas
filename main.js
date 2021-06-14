af11fdbe980503342d608d5d56b0207bfa96130e
nabreu <n.abreu.s22@gmail.com>
2021-06-14 18:03:16 -0300

    criação do arquivo  JSe execução de exercícios

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function clicou (){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigada por clicar</b>";
+   // alert("Obrigado por clicar");
+}
+
+function redirecionar (){
+    window.open("https://github.com/nabreu22/aulas_javascript");
+}
+
+
+/*function validaIdade(idade){
+    var validar = ;
+    if(idade >= 18){
+        validar = true
+} else { 
+        validar = false    
+}
+    return validar
+}
+
+let idade = prompt(" Qual a sua idade? ");
+console.log(validaIdade(idade));
+*/
